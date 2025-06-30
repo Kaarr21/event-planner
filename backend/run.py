62a5730f588030888823eaf1aa484354bd7493f8
Karoki <wanjikukar19@gmail.com>
2025-06-30 11:21:42 +0300

    migrations

--- backend/run.py
+++ backend/run.py
@@ -4,7 +4,7 @@ from app import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    #with app.app_context():
+        #db.create_all()
     app.run(debug=True)
     
